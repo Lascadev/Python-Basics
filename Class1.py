f8d1d087029b3c72ae35523e5b0501660d1fa27c
Triusdeil <34854558+triusdeil@users.noreply.github.com>
2024-08-31 11:47:54 -0400

    clases practicas de py 1

---
+++ Class1.py
@@ -0,0 +1,22 @@
+'Nombre de una variable'
+'no se puede comenzar por un numero'
+'no puede contener espacios'
+'tampoco simbolos especiales'
+'podemos utilizar _ para los espacios'
+'numbers int'
+num1 = 3 
+num2 = 3
+result1 = num1 + num2
+result2 = num1 - num2
+result3 = num1 * num2
+result4 = num1 / num2
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
+nota_1 = 5
+nota_2 = 10
+nota_total = nota_1 * 0.40 + nota_2 * 0.6
+print(nota_total)
+
